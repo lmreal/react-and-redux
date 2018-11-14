bb8cfd2cef42dfc675033a44ae7ef95714c86232
lmreal <1071410797@qq.com>
2018-11-14 20:55:57 +0800

    Update ControlPanel.js
    应该是CounterContainer

--- chapter-03/redux_smart_dumb/src/views/ControlPanel.js
+++ chapter-03/redux_smart_dumb/src/views/ControlPanel.js
@@ -10,9 +10,9 @@ class ControlPanel extends Component {
   render() {
     return (
       <div style={style}>
-        <Counter caption="First" />
-        <Counter caption="Second" />
-        <Counter caption="Third" />
+        <CounterContainer caption="First" />
+        <CounterContainer caption="Second" />
+        <CounterContainer caption="Third" />
         <hr/>
         <Summary />
       </div>
